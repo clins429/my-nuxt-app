90e6f09ad8efd86f8584d531e36be431fee1a446
Caio Lins <wolflow429@gmail.com>
2023-04-12 13:44:20 +0900

    playing around with routes

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>Hello Nuxt 3</div>
+  <div>
+    <NuxtLayout>
+      <header>
+        <ul>
+          <li><NuxtLink to="/">home</NuxtLink></li>
+          <li><NuxtLink to="/events">events</NuxtLink></li>
+          <li><NuxtLink to="/events/itsme">itsme event</NuxtLink></li>
+        </ul>
+      </header>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
---
+++ pages/events/[id].vue
@@ -0,0 +1,9 @@
+<script setup>
+const route = useRoute()
+console.log('params', route.params)
+// const nuxt = useNuxtApp() // holds the app context
+</script>
+
+<template>
+  <div>event id: {{ $route.params.id }}</div>
+</template>
